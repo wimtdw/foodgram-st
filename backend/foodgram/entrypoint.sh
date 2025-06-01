559b35156e936fc1456005db195714da95e5f3ff
Anna Pankratova <153401722+wimtdw@users.noreply.github.com>
2025-06-01 22:28:32 +0300

    Fix backend Dockerfile

--- backend/foodgram/entrypoint.sh
+++ backend/foodgram/entrypoint.sh
@@ -12,7 +12,7 @@ else
 fi
 
 echo "Collecting static files..."
-python manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput
 
 echo "Starting Gunicorn server..."
 exec gunicorn --bind 0.0.0.0:8000 foodgram.wsgi
